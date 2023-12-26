ffc7e0765defab8fc0949da2cf914dcf8932d3bc
andrey <andsul@mail.ru>
2023-12-26 17:28:48 +0300

    delete rows and more

--- test_pandas.py
+++ test_pandas.py
@@ -4,12 +4,30 @@ import openpyxl
 
 # Открытие xls файла
 df_data = pd.read_excel('data_in/testPandas.xls')
-# Список заголовков столбцов листа
-list_titles = df_data.columns.ravel()
-print(list_titles)
-# Данные столбца
-data_row = df_data['Контрагент']
-print(data_row)
+
+# Инфо датафрейма
+df_data.info()
+
+# # Выбор по строкам и столбцам
+# print(df_data.iloc[4, 8])
+
+# # Статистика по числовым столбцам
+# df_data.describe()
+
+# # Список заголовков столбцов листа
+# list_titles = df_data.columns
+# print(list_titles)
+
+# # Данные столбца
+# data_row = df_data['Контрагент'] # или data_row = df_data.Контрагент
+# print(data_row)
+
+# Удаление столцов
+del_list = ['Дата','Номер','Вид документа', 'Вид операции', 'Информация', 'Организация',
+            'Ответственный', 'Комментарий']
+for i in del_list:
+    del df_data[i]
+print(df_data.head())
 
 # Открытие xlsx файла через xlrd
 # file_XLRD = xlrd.open_workbook('data_in/testPandas.xlsx', formatting_info=True)

4993fad3e3a43af50b9b54d865809d51da593088
andrey <andsul@mail.ru>
2023-12-29 15:21:09 +0300

    revision of the class

--- prog/incomingTable.py
+++ prog/incomingTable.py
@@ -3,31 +3,50 @@ import configparser
 
 
 class IncomingTable:
-    """ Создание таблицы из файла .xls.
-     path - путь и название файла.
-     get_table - возвращает таблицу из файла."""
+    """
+    Создание таблицы из файла .xls.
+    path - путь и название файла.
+    name_seller - поставщик.
+    get_table - возвращает таблицу из файла.
+    get_name_seller - возвращает название поставщика.
+    """
 
     def __init__(self, path):
+        self.__config = configparser.ConfigParser()
+        self.__config.read('config.ini', encoding='utf-8')
+        self.__table = pd.read_excel(path)
         self.__name_seller = None
-        self.__path = path
-        self.__table = self.processing()
+        self.processing()
+        # self.__path = path
+        # self.__table = None
 
     def get_table(self):
         return self.__table
 
+    def get_name_seller(self):
+        return self.__name_seller
+
     def processing(self):
         self.assign_name()
-        return pd.read_excel(self.__path)
+        if self.__name_seller == None:
+            self.__table = None
+            return
+        self.delete_columns()
 
     def assign_name(self):
-        config = configparser.ConfigParser()
-        config.read('config.ini')
+        # config = configparser.ConfigParser()
+        # config.read('config.ini', encoding='utf-8')
 
-        table_in = pd.read_excel(self.__path)
-        str_in = ''.join(table_in.columns)
-        for seller in config['compare']:
-            if config['compare'][seller] == str_in:
-                self.__name_seller = seller
-            pass
-        # sellers = [seller for seller in config['compare'] if config['compare'][seller] == str_in]
-        # print(sellers)
+        # table_in = pd.read_excel(self.__path)
+        str_in = ''.join(self.__table.columns)
+        for key in self.__config['compare']:
+            if self.__config['compare'][key] == str_in:
+                self.__name_seller = key
+                break
+
+    def delete_columns(self):
+        columns_leave = list(self.__config['columns'][self.__name_seller].split(','))
+        columns_file = list(self.__table.columns)
+        for i in columns_file:
+            if i not in columns_leave:
+                del self.__table[i]
--- test_Andrey.py
+++ test_Andrey.py
@@ -4,9 +4,15 @@ import configparser
 
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('config.ini', encoding='utf-8')
 
-for filenamme in os.listdir(config['DEFAULT']['path'] + '/'):
+for filenamme in os.listdir(config['other']['path'] + '/'):
     if filenamme.endswith('.xls'):
         table = IncomingTable(f'data_in/{filenamme}').get_table()
-        print (''.join(table.columns))
+        print(''.join(table.columns))
+        print(table.get_name_seller)
+
+# for key in config['compare']:
+#     print(key + ': ' + config['compare'][key])
+
+# key = [key for key in config['compare'] if config['compare'][key] == str_in]

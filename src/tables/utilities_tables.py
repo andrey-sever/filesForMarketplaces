8f213e906db2268cdd34b2079e39e7a08993181e
andrey <andsul@mail.ru>
2024-01-15 17:40:24 +0300

    add def add_balance_price

--- src/tables/utilities_tables.py
+++ src/tables/utilities_tables.py
@@ -13,3 +13,14 @@ def merge_tables(tables: list[pd.DataFrame]):
             continue
         new_table = pd.concat([new_table, table], sort=False, axis=0, ignore_index=True)
     return new_table
+
+def add_balance_price(path_file_in: str, general_price_list: pd.DataFrame):
+    """
+    Добавление в файл остатка и цены. Можно по отдельности.
+    :param path_file_in: путь к файлу, куда добавлять
+    :param general_price_list: общий прайс лист в формате DataFrame
+    :return: сохраняет файл по тому же пути
+    """
+    df_in = pd.read_excel(path_file_in)
+    for index, row in df_in.iterrows():
+        print(row['Артикул'])
--- src/test_Andrey.py
+++ src/test_Andrey.py
@@ -2,7 +2,7 @@ from tables.incomingTable import IncomingTable
 import os
 import configparser
 
-from tables.utilities_tables import merge_tables
+from tables.utilities_tables import merge_tables, add_balance_price
 
 config = configparser.ConfigParser()
 config.read('incoming.ini', encoding='utf-8')
@@ -13,12 +13,15 @@ for filename in os.listdir(path):
     if filename.endswith('.xls'):
         incTable = IncomingTable(f'{path}{filename}')
         table = incTable.get_table()
-        list_tables.append(table) #сохранить в список
-        print(f'{incTable.get_name_seller()}: {table.columns}')
-        print(table.info)
+        if table is not None:
+            list_tables.append(table) #сохранить в список
+            # print(f'{incTable.get_name_seller()}: {table.columns}')
+            # print(table.info)
 newTable = merge_tables(list_tables)                    #
-newTable.to_excel('/home/andrey/new_table.xlsx', index=False)    # Соединенные таблицы и сброс в excel
-print(newTable.info)                                    #
+# newTable.to_excel('/home/andrey/new_table.xlsx', index=False)    # Соединенные таблицы и сброс в excel
+# print(newTable.info)
+
+add_balance_price('data_in/Шаблон.xls', newTable)
 
 # for key in config['compare']:
 #     print(key + ': ' + config['compare'][key])
